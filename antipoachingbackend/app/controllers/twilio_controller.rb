f7a7ed74ffa58e5b9d4b42aa7f36ea9518a3b1a0
Zachary Goldstein <zachgold@gmail.com>
2015-07-23 15:33:13 +0200

    Adding an sms endpoint for the app.

---
+++ antipoachingbackend/app/controllers/twilio_controller.rb
@@ -0,0 +1,36 @@
+require 'twilio-ruby'
+
+class TwilioController < ApplicationController
+  include Webhookable
+
+  after_filter :set_header
+
+  skip_before_action :verify_authenticity_token
+
+  def sms
+    @account_sid = ENV["TWILIO_SID"]
+    @auth_token = ENV["TWILIO_AUTH"]
+
+    @client = Twilio::REST::Client.new(@account_sid, @auth_token)
+    @account = @client.accounts.get(@account_sid)
+
+    if !params.has_key?(:From) || !params.has_key?(:Body)
+      render status: 400, json: {reason:'No from or body param'}.to_json
+      return
+    end
+
+    report = PoachingReport.create do |report|
+      report.raw_sms = params["Body"]
+      report.is_clean = false
+      report.sms_number = params["From"]
+    end
+
+    @account.messages.create(
+        from: '+12562724563',
+        to: params["From"],
+        body: 'Thank you for reporting this poaching incident'
+    )
+
+    render status: 200, json: report.to_json
+  end
+end
