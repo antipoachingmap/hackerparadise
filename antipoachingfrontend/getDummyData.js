41ea1c00abc1b28b350c412151a3f46511dc594a
Talii123 <tfriedman@gmail.com>
2015-07-23 18:54:03 +0200

    add filter by species

--- antipoachingfrontend/getDummyData.js
+++ antipoachingfrontend/getDummyData.js
@@ -43,7 +43,27 @@ function getDummyData() {
         "created_at": "2015-07-23T12:36:26.456Z",
         "updated_at": "2015-07-23T12:36:26.456Z"
       }
-
+      , {
+        "id": 56,
+        "raw_sms": null,
+        "latitude": -17.7639,
+        "longitude":  30.0297,
+        "location": null,
+        "species": "Elephant",
+        "victim_count": 5,
+        "event_datetime": "2015-07-23T11:36:26.456Z",
+        "description": "A family of elephants",
+        "photos": [
+          
+        ],
+        "submission_datetime": "2015-07-23T12:36:26.456Z",
+        "submission_channel": null,
+        "is_clean": null,
+        "cleaned_by": null,
+        "locked": null,
+        "created_at": "2015-07-23T12:36:26.456Z",
+        "updated_at": "2015-07-23T12:36:26.456Z"
+      }
       , {
         "id": 75,
         "raw_sms": null,
