4035247ea3b4f87b76c66faf3f93b2b6d13bbcf8
Zachary Goldstein <zachgold@gmail.com>
2015-07-23 19:30:37 +0200

    best effort at parsing message out of text

--- antipoachingbackend/app/controllers/twilio_controller.rb
+++ antipoachingbackend/app/controllers/twilio_controller.rb
@@ -15,14 +15,31 @@ class TwilioController < ApplicationController
     @account = @client.accounts.get(@account_sid)
 
     if !params.has_key?(:From) || !params.has_key?(:Body)
-      render status: 400, json: {reason:'No from or body param'}.to_json
+      render status: 400, json: {reason: 'No from or body param'}.to_json
       return
     end
 
+
     report = PoachingReport.create do |report|
+
+      msg = params["Body"]
+      eventParts = msg.split(',')
+
+      if (eventParts.length >= 3)
+
+        report.latitude, report.longitude = eventParts[0].split(' ')
+
+        report.victim_count, report.species = eventParts[1].split(' ')
+
+        report.description = eventParts[2]
+      end
+
       report.raw_sms = params["Body"]
       report.is_clean = false
       report.sms_number = params["From"]
+
+
+
     end
 
     @account.messages.create(
@@ -32,5 +49,7 @@ class TwilioController < ApplicationController
     )
 
     render status: 200, json: report.to_json
+
   end
+
 end
