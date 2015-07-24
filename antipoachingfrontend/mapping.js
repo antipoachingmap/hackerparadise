077723e4c4288a40abab6272a194657486f04f72
Talii123 <tfriedman@gmail.com>
2015-07-24 13:55:39 +0200

    add handling to skip invalid entries

--- antipoachingfrontend/mapping.js
+++ antipoachingfrontend/mapping.js
@@ -15,10 +15,11 @@
 		, map
 		, popup
 		, speciesLayers = {}
+		, HARARE_LAT_LNG = [-17.8639, 31.0297]
 		, USE_DUMMY_DATA = false
 		;
 	
-	map = L.map('map', {scrollWheelZoom: false}).setView([-17.8639, 31.0297], 9);
+	map = L.map('map', {scrollWheelZoom: false}).setView(HARARE_LAT_LNG, 9);
 	popup = L.popup();
 
 	L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6IjZjNmRjNzk3ZmE2MTcwOTEwMGY0MzU3YjUzOWFmNWZhIn0.Y8bhBaUMqFiPrDRW9hieoQ', {
@@ -29,27 +30,30 @@
 
 	$.when(getMarkers()).then(onMarkers);
 
+
 	function onMarkers(data) {
-		$.each(data, function(index, entry) {
+		$(data).filter(ensureRequiredFields).each(function(index, post) {
 			var ICON_BASE_URL = 'http://antipoachingmap.org/wp-content/uploads/2015/07/#{species}.png'
 				, icon
-				, species = (entry.species || "").trim()
+				, species = post.species
 				, layer
-				, useCustomIcon = true && species.length && species !== 'Other'
+				, useCustomIcon
 				;
-				
+
+			// remove code using 'useCustomIcon' if we have a default icon corresponding to 'Other' species				
+			useCustomIcon = species.length && species !== 'Other'
 			if (useCustomIcon) {
 				icon = L.icon({
 					iconUrl: ICON_BASE_URL.replace('#{species}', species)
-					, iconAnchor: [32, 64]
+					, iconAnchor: [32, 64]	// our icons have a different origin (0,0) then the out of the box ones
 				});
-				marker = L.marker([entry.latitude, entry.longitude], {icon: icon});
+				marker = L.marker([post.latitude, post.longitude], {icon: icon});
 			}
 			else {
-				marker = L.marker([entry.latitude, entry.longitude]);
+				marker = L.marker([post.latitude, post.longitude]);
 			}
 	
-			layer = speciesLayers[species]
+			layer = speciesLayers[species];
 			if (layer) {
 				layer.push(marker);
 			} 
@@ -60,7 +64,7 @@
 			//marker.addTo(map)
 			marker.bindPopup(popup, {offset: [0, -44]})
 				.on('click', function(e) {
-					marker.setPopupContent(makePopupContent(entry));
+					marker.setPopupContent(makePopupContent(post));
 				});
 		});	
 		
@@ -81,9 +85,9 @@
 			"<tr><td colspan='2'>" + value + "</td></tr>" ;
 	}
 
-	function makePopupContent(entry) {
+	function makePopupContent(post) {
 		var fieldsToShow = $.map(POPUP_FIELDS, function(label, field) {
-			value = entry[field];
+			value = post[field];
 			return value ? formatter(label, value) : null;
 		});
 
@@ -97,6 +101,36 @@
 		return !USE_DUMMY_DATA ? 
 			$.getJSON(data_url, onMarkers) : 
 			getDummyData();
+	}
+
+	function ensureRequiredFields(index, post) {
+		var REQUIRED_FIELDS = ['latitude', 'longitude', 'species']
+			, missingFields = []
+			, emptyFields = []
+			, isInvalid = false;
+
+		$.each(REQUIRED_FIELDS, function(index, field) {
+			var value = post[field];
+			if (!value) {
+				missingFields.push(field);
+			}
+			else if (typeof value === 'string' && !value.trim().length) {
+				emptyFields.push(field);
+			}
+		});
+
+		if (missingFields.length) {
+			isInvalid = true;
+			console.log('Skipping post because it is missing the following fields: ', missingFields);
+			console.dir(post);
+		}
+		if (emptyFields.length) {
+			isInvalid = true;
+			console.log('Skipping post because the following fields are empty: ', emptyFields);
+			console.dir(post);
+		}
+		
+		return !isInvalid;
 	}
 
 })();
